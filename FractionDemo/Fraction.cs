279f64e58ccaf4fe4edfc1d7af80242a32187b6a
sanczoNB <sanczo16@gmail.com>
2017-01-17 22:06:00 +0100

    Zdefiniowanie konstruktora struktury fraction oraz dodanie stałych z charkterystycznymi ułamkami

--- FractionDemo/Fraction.cs
+++ FractionDemo/Fraction.cs
@@ -1,9 +1,26 @@
-﻿namespace FractionDemo
+﻿using System;
+
+namespace FractionDemo
 {
     public struct Fraction
     {
         private int _numerator;
 
         private int _denominator;
+
+        public Fraction(int numerator, int denominator = 1)
+        {
+            if (denominator == 0)
+            {
+                throw new ArgumentException("Denominator must not be equal zero");
+            }
+            _denominator = denominator;
+            _numerator = numerator;
+        }
+
+        public static readonly Fraction Zero = new Fraction(0);
+        public static readonly Fraction One = new Fraction(1);
+        public static readonly Fraction Half = new Fraction(1,2);
+        public static readonly Fraction Quater = new Fraction(1,4);
     }
 }
