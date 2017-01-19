c124d92492a8fde8fba3b8ce5468ef8d7002d304
sanczoNB <sanczo16@gmail.com>
2017-01-19 20:47:13 +0100

    dodano metody: uprość i ToString

--- FractionDemo/Fraction.cs
+++ FractionDemo/Fraction.cs
@@ -14,8 +14,61 @@ namespace FractionDemo
             {
                 throw new ArgumentException("Denominator must not be equal zero");
             }
-            _denominator = denominator;
             _numerator = numerator;
+            _denominator = denominator;
+        }
+
+        public override string ToString()
+        {
+            return $"{_numerator}/{_denominator}";
+        }
+
+        public double ToDouble()
+        {
+            return _numerator/(double) _denominator;
+        }
+
+        public void SimplifyNaiveVersion()
+        {
+            var greatestCommonDivisor = GreatestCommonDivisorWithEuclideanAlgorithm();
+            _numerator = _numerator/greatestCommonDivisor;
+            _denominator = _denominator/greatestCommonDivisor;
+            
+            //sign
+            if (_numerator * _denominator < 0)
+            {
+                _numerator = - Math.Abs(_numerator);
+                _denominator = Math.Abs(_denominator);
+            }
+            else
+            {
+                _numerator = Math.Abs(_numerator);
+                _denominator = Math.Abs(_denominator);
+            }
+        }
+
+        public int GreatestCommonDivisorWithEuclideanAlgorithm()
+        {
+            int a = Math.Max(_denominator, _numerator);
+            int b = Math.Min(_denominator, _numerator);
+            do
+            {
+                var c = a%b;
+                a = b;
+                b = c;
+            } while (b != 0);
+
+            return a;
+        }
+
+        private int GreatestCommonDivisorNaiveVersion()
+        {
+            var greatestCommonDivisor = Math.Min(Math.Abs(_numerator), Math.Abs(_denominator));
+            while ((_numerator % greatestCommonDivisor != 0) || (_denominator % greatestCommonDivisor != 0))
+            {
+                greatestCommonDivisor--;
+            }
+            return greatestCommonDivisor;
         }
 
         public static string Info()
--- FractionDemo/Program.cs
+++ FractionDemo/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,20 @@ namespace FractionDemo
     {
         static void Main(string[] args)
         {
+            Fraction fractionTwo = new Fraction(2, 1);
+            Fraction fractionZero = Fraction.Zero;
+            Fraction fractionHalf = Fraction.Half;
+            Console.WriteLine(fractionHalf.ToString());
+
+            Fraction fraction = new Fraction(4,-2);
+            fraction.SimplifyNaiveVersion();
+            Console.WriteLine(fraction);
+
+            Console.WriteLine(Fraction.Info());
+            
+            Console.ReadKey();
         }
+
+            
     }
 }
