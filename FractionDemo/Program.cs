27c3d615bedff01458b238e63730ebb52cdf041e
sanczoNB <sanczo16@gmail.com>
2017-01-21 10:36:28 +0100

    test sortowania

--- FractionDemo/Program.cs
+++ FractionDemo/Program.cs
@@ -6,11 +6,36 @@ namespace FractionDemo
     {
         static void Main(string[] args)
         {
-            var r = (double) Fraction.Half;
-            Console.WriteLine(r);
-            Fraction f = 2;
-            Console.WriteLine(f);
-                       
+            Fraction[] tab = new Fraction[10];
+            for (int i = 0; i < tab.Length; i++)
+            {
+                tab[i] = new Fraction(1, i + 1);
+            }
+
+            Console.WriteLine("Przed sortowaniem: ");
+            foreach (var f in tab)
+            {
+                Console.WriteLine(f + " = " + f.ToDouble());
+            }
+
+            try
+            {
+                Array.Sort(tab);
+            }
+            catch (Exception exc)
+            {
+
+                ConsoleColor currentColor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(exc.Message);
+                Console.ForegroundColor = currentColor;
+            }
+
+            Console.WriteLine("\nPo sortowaniu: ");
+            foreach (var f in tab)
+            {
+                Console.WriteLine("{0} = {1}", f, f.ToDouble());
+            }
             Console.ReadKey();
         }
 
